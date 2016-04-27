b9a06223d8461bad26706f5ef94c6ca64dd50070
jinsen47 <jinsen47@gmail.com>
2016-04-27 23:30:50 +0800

    头条
    Signed-off-by: jinsen47 <jinsen47@gmail.com>

--- src/com/github/jinsen47/Main.java
+++ src/com/github/jinsen47/Main.java
@@ -1,26 +1,52 @@
 package com.github.jinsen47;
+
 import java.util.*;
 public class Main{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        while (in.hasNextInt()) {
-            int t = in.nextInt();
-            List<Boolean> results = new ArrayList<>();
-            for (int i = 0; i < t; i++) {
-                boolean result = solution(in.nextInt(), in.nextInt(), in.nextInt());
-                results.add(result);
+        while (in.hasNext()) {
+            int n = in.nextInt();
+            int value = in.nextInt();
+            String[] strings = new String[n];
+            for (int i = 0; i < n; i++) {
+                strings[i] = in.next();
             }
-            for (int i = 0; i < results.size(); i++) {
-                boolean b = results.get(i);
-                System.out.println(String.format("Case #%d: ", i+1) + b);
-            }
+            System.out.println(solution(strings, value));
         }
     }
 
-    public static boolean solution(int a, int b, int c) {
-        long aLong = a;
-        long bLong = b;
-        long cLong = c;
-        return aLong + bLong > cLong;
+    public static int solution(String[] strings, int value) {
+        int count = 0;
+        for (String s : strings) {
+            int singleCount = 1;
+            for (int i = 1; i <= s.length(); i++) {
+                String shift = shiftLeft(s, i);
+                if (shift.equals(s)) {
+                    singleCount++;
+                }
+            }
+            if (singleCount == value) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static String shiftLeft(String str, int times) {
+        LinkedList<Character> list = new LinkedList<>();
+        char[] array = str.toCharArray();
+        for (int i = 0; i < array.length; i++) {
+            list.add(array[i]);
+        }
+
+        for (int i = 0; i < times; i++) {
+            char c = list.remove(0);
+            list.add(c);
+        }
+        StringBuilder sb = new StringBuilder(str.length());
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i));
+        }
+        return sb.toString();
     }
 }
